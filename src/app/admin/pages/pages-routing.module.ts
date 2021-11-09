5cb7121e514d394d0b2440b8d3892d525f38cacc
shahjalalcse <shah-jalal1>
2021-11-09 13:00:22 +0600

    admin routing done

--- src/app/admin/pages/pages-routing.module.ts
+++ src/app/admin/pages/pages-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PagesComponent} from "./pages.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
