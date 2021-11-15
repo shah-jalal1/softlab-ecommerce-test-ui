f238e3e1270776f5b79866fd76b94b78891b6187
shah-jalal1 <shah.jalal0894@gmail.com>
2021-11-15 13:41:15 +0600

    get all categories

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfirmDialogeComponent } from './components/ui/confirm-dialoge/confirm-dialoge.component';
 import { SnackbarNotificationComponent } from './components/ui/snackbar-notification/snackbar-notification.component';
-
+import { ConfirmDailogComponent } from './components/ui/confirm-dailog/confirm-dailog.component';
+import {PipesModule} from './pipes/pipes.module';
 
 
 @NgModule({
   declarations: [
     ConfirmDialogeComponent,
-    SnackbarNotificationComponent
+    SnackbarNotificationComponent,
+    ConfirmDailogComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    PipesModule
   ]
 })
 export class SharedModule { }
