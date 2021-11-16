db2a2f46a48608827ae9c9622f39a085a58b6c29
shah-jalal1 <shah.jalal0894@gmail.com>
2021-11-16 12:49:54 +0600

    emedilife header added

--- src/app/core/menu/menu.module.ts
+++ src/app/core/menu/menu.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import {MaterialModule} from "../../material/material.module";
+import {FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 
 
@@ -12,7 +14,10 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class MenuModule { }
